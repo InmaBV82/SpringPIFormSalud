c52dc07fab369e4a507e8f564ed2fa8c8c916840
Inma Borrego <ibvelarde@gmail.com>
2024-05-09 18:44:34 +0200

    Añado las posibles llamadas desde el front al cors para que no me bloquee

--- src/main/java/com/ejemplos/configuracion/Configuracion.java
+++ src/main/java/com/ejemplos/configuracion/Configuracion.java
@@ -22,7 +22,10 @@ public class Configuracion {
 	    return new WebMvcConfigurer() {
 	        @Override
 	        public void addCorsMappings(CorsRegistry registry) {
-	            registry.addMapping("/**").allowedOrigins("http://localhost:4200");
+	        	 registry.addMapping("/**")
+	                .allowedOrigins("http://localhost:4200")
+	                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")  // Include OPTIONS for pre-flight requests
+	                .allowedHeaders("*");
 	        }
 	    };
 	}
